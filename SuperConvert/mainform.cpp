6444c5bb0cb45c5429f0eca1dc44e84a241beeb5
sqawkingapple <stevenning.203@gmail.com>
2020-07-02 10:14:03 -0600

    Initial files

---
+++ SuperConvert/mainform.cpp
@@ -0,0 +1,13 @@
+#include "mainform.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    //WinformCDemo is your project name
+    SuperConvert::mainform form;
+    Application::Run(% form);
+}
