cc8888d8d3e5bc21eb28b4a9728e6948cce9e514
sqawkingapple <stevenning.203@gmail.com>
2020-08-04 20:32:52 -0600

    Removed Octal and finished binary and decimal and hexidecimal conversion process. Added error checking

--- SuperConvert/basicStringConvert.h
+++ SuperConvert/basicStringConvert.h
@@ -13,3 +13,28 @@ System::String^ charToSystemString(char* bufferToConvert)
 	return convertedSystemString;
 }
 
+int lengthOf(char c)
+{
+	if (c != NULL)
+		return 1;
+	else
+		return 0;
+}
+
+int lengthOf(std::string c)
+{
+	int length = c.length();
+	return length;
+}
+
+int lengthOf(char *c)
+{
+	std::string convertedString = (std::string)c;
+	return convertedString.length();
+}
+
+int lengthOf(int c)
+{
+	int size = std::to_string(c).length();
+	return size;
+}
--- SuperConvert/mainform.h
+++ SuperConvert/mainform.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <cctype>
 #include "basicStringConvert.h"
+#include "math.h"
+#include <msclr\marshal_cppstd.h>
 
 namespace SuperConvert {
 
@@ -47,11 +49,12 @@ namespace SuperConvert {
 	private: System::Windows::Forms::TextBox^ inputTextBox;
 
 	private: System::Windows::Forms::Button^ button4;
-	private: System::Windows::Forms::Button^ button5;
-	private: System::Windows::Forms::Button^ button6;
+
+
 	private: System::Windows::Forms::TextBox^ outputTextBox;
 
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Label^ label1;
 
 	protected:
 
@@ -74,25 +77,25 @@ namespace SuperConvert {
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->inputTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->button5 = (gcnew System::Windows::Forms::Button());
-			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->outputTextBox = (gcnew System::Windows::Forms::TextBox());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(118, 99);
+			this->button1->Location = System::Drawing::Point(118, 125);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(131, 23);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"Bin to Dec";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &mainform::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(118, 12);
+			this->button2->Location = System::Drawing::Point(118, 38);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(131, 23);
 			this->button2->TabIndex = 1;
@@ -102,7 +105,7 @@ namespace SuperConvert {
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(118, 41);
+			this->button3->Location = System::Drawing::Point(118, 67);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(131, 23);
 			this->button3->TabIndex = 2;
@@ -119,30 +122,13 @@ namespace SuperConvert {
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(118, 70);
+			this->button4->Location = System::Drawing::Point(118, 96);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(131, 23);
 			this->button4->TabIndex = 4;
 			this->button4->Text = L"Dec to Bin";
 			this->button4->UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this->button5->Location = System::Drawing::Point(118, 128);
-			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(131, 23);
-			this->button5->TabIndex = 6;
-			this->button5->Text = L"Dec to Oct";
-			this->button5->UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this->button6->Location = System::Drawing::Point(118, 157);
-			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(131, 23);
-			this->button6->TabIndex = 5;
-			this->button6->Text = L"Oct to Dec";
-			this->button6->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &mainform::button4_Click);
 			// 
 			// outputTextBox
 			// 
@@ -156,19 +142,27 @@ namespace SuperConvert {
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
 			this->pictureBox1->Location = System::Drawing::Point(12, 38);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(100, 112);
+			this->pictureBox1->Size = System::Drawing::Size(100, 115);
 			this->pictureBox1->TabIndex = 8;
 			this->pictureBox1->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(130, 159);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(106, 13);
+			this->label1->TabIndex = 9;
+			this->label1->Text = L"Version: 1.0a release";
 			// 
 			// mainform
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(261, 193);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->outputTextBox);
-			this->Controls->Add(this->button5);
-			this->Controls->Add(this->button6);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->inputTextBox);
 			this->Controls->Add(this->button3);
@@ -190,9 +184,22 @@ namespace SuperConvert {
 		if (testFieldNotEmpty != nullComparator)
 		{
 			char buffer[99];
-			int input = Convert::ToInt64(inputTextBox->Text);
-			itoa(input, buffer, 16);
-			outputTextBox->Text = Convert::ToString(charToSystemString(buffer));
+			msclr::interop::marshal_context context;
+			std::string tester = context.marshal_as<std::string>(inputTextBox->Text);
+			for (int i = 0; i < tester.length(); i++)
+			{
+				if (!isdigit(tester[i]) || tester.length() > 16)
+				{
+					MessageBox::Show("Input is not numeric, or input is too large.", "Error");
+					break;
+				}
+				else
+				{
+					int input = Convert::ToInt64(inputTextBox->Text);
+					itoa(input, buffer, 16);
+					outputTextBox->Text = Convert::ToString(charToSystemString(buffer));
+				}
+			}
 		}
 	}
 	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
@@ -200,7 +207,87 @@ namespace SuperConvert {
 		String^ nullComparator = "";
 		if (testFieldNotEmpty != nullComparator)
 		{
-
+			unsigned int buffer = 0;
+			String^ input = Convert::ToString(inputTextBox->Text);
+			unsigned char n = input->Length;
+			for (int i = 0; i < n; i++)
+			{
+				String^ tString = input;
+				int tChar = 0;
+				if (tString[i] >= 48 && tString[i] <= 57)
+					tChar = (int)(tString[i] - '0');
+				else if (tString[i] >= 65 && tString[i] <= 90)
+				{
+					tChar = (int)(tString[i] - '7');
+				}
+				else if (tString[i] >= 97 && tString[i] <= 122)
+				{
+					tChar = (int)(tString[i] - 'W');
+				}
+				else
+				{
+					buffer = 0;
+					break;
+				}
+				buffer += tChar * pow(16, n - i - 1);
+			}
+			outputTextBox->Text = Convert::ToString(buffer);
+		}
+	}
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) { // dec to bin
+		String^ testFieldNotEmpty = inputTextBox->Text;
+		String^ nullComparator = "";
+		if (testFieldNotEmpty != nullComparator)
+		{
+			char buffer[99];
+			msclr::interop::marshal_context context;
+			std::string tester = context.marshal_as<std::string>(inputTextBox->Text);
+			for (int i = 0; i < tester.length(); i++)
+			{
+				if (!isdigit(tester[i]) || tester.length() > 16)
+				{
+					MessageBox::Show("Input is not numeric, or input is too large.", "Error");
+					break;
+				}
+				else
+				{
+					int input = Convert::ToInt64(inputTextBox->Text);
+					itoa(input, buffer, 2);
+					outputTextBox->Text = Convert::ToString(charToSystemString(buffer));
+				}
+			}
+		}
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) { // bin to dec
+		String^ testFieldNotEmpty = inputTextBox->Text;
+		String^ nullComparator = "";
+		if (testFieldNotEmpty != nullComparator)
+		{
+			char buffer[99];
+			msclr::interop::marshal_context context;
+			std::string tester = context.marshal_as<std::string>(inputTextBox->Text);
+			for (int i = 0; i < tester.length(); i++)
+			{
+				if (!isdigit(tester[i]) || tester.length() > 16 || tester[i] - 48 > 1)
+				{
+					MessageBox::Show("Input is not binary, or input is too large.", "Error");
+					break;
+				}
+				else
+				{
+					unsigned int buffer = 0;
+					String^ input = Convert::ToString(inputTextBox->Text);
+					unsigned char n = input->Length;
+					for (int i = 0; i < n; i++)
+					{
+						String^ tString = input;
+						int tChar = tString[i] - 48;
+						buffer += tChar * pow(2, n - i - 1);
+						outputTextBox->Text = Convert::ToString(buffer);
+					}
+				}
+			}
+				
 		}
 	}
 };
